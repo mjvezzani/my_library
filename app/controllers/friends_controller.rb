740293743c25b3858aeb30f14e46b05a681e2256
Michael Vezzani <mjvezzani@gmail.com>
2014-09-03 14:54:49 -0400

    Create friend _form.html.erb and new.html.erb. Enable entry of friends to the database.

---
+++ app/controllers/friends_controller.rb
@@ -0,0 +1,28 @@
+class FriendsController < ApplicationController
+
+  def show
+    @friend = Friend.find(params[:id])
+  end
+
+  def new
+    @friend = Friend.new()
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+
+    if @friend.save
+      flash[:success] = 'Friend added'
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def friend_params
+    params.require(:friend).permit(:first_name, :last_name, :address, :phone, :user_id)
+  end
+
+end
